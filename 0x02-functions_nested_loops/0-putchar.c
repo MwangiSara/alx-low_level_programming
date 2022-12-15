e4d17575f802730063a046827128ceddf6fb537c
sarah Mwangi <114574064+MwangiSara@users.noreply.github.com>
2022-12-15 22:39:41 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,27 @@
 /*
  * File: 0-holberton.c
- * Auth: Brennan D Baraban
+ * Auth: Sarah
  *
  */
 
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - Prints "_putchar" followed by a new line.
+ * main - Prints "Holberton" followed by a new line.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
 	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-  _putchar('r');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
