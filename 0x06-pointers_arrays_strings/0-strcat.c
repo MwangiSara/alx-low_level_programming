34f059d8c6f6141c63c27f07ef1ee2b51a3bfc8b
sarah Mwangi <114574064+MwangiSara@users.noreply.github.com>
2022-12-23 14:38:05 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,23 @@
-#include "main.h"
-
-/**
- * *_strcpy - See description
- * @dest: char type string
- * @src: char type string
- * Description: Copy the string pointed to by pointer `src` to
- * the buffer pointed to by `dest`
- * Return: Pointer to `dest`
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
-	return (dest);
+#include "main.h"                                                                                                                    
+                                                                                                                                     
+/**                                                                                                                                  
+ * strcat - Concatenates the string pointed to by @src, including the x                                                     
+ *          null byte, to the end of the string pointed to by @dest.                                                                 
+ * @dest: A pointer to the string to be concatenated upon.                                                                           
+ * @src: The source string to be appended to @dest.                                                                                  
+ *                                                                                                                                   
+ * Return: A pointer to the destination string @dest.                                                                                
+ */                                                                                                                                  
+char *_strcat(char *dest, const char *src)                                                                                            
+{                                                                                                                                    
+        int index = 0, dest_len = 0;                                                                                                 
+                                                                                                                                     
+        while (dest[index++])                                                                                                        
+                dest_len++;                                                                                                          
+                                                                                                                                     
+        for (index = 0; src[index]; index++)                                                                                         
+                dest[dest_len++] = src[index];                                                                                       
+                                                                                                                                     
+        return (dest);                                                                                                               
 }
+
